48e0736e4bd6f8700dda2aeca5a39199b2225897
Vladislav Isakov <bladebhs@gmail.com>
2018-10-20 18:00:18 +0500

    Add FactoryBot and spec_helper

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
 
+gem 'factory_bot'
 gem 'rspec', '~> 3.0'
 gem 'rubocop', '~> 0.59.2', require: false
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,12 @@
+require 'factory_bot'
+
 RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+
+  config.before(:suite) do
+    FactoryBot.find_definitions
+  end
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
